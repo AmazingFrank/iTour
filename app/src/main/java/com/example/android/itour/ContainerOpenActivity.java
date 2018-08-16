1e65d2292acad04e1dc63098a70db8d0732d2d0e
Cavdy <cavdyofficials@gmail.com>
2018-08-16 12:29:59 +0100

    Added New Actvities and Java Classes

--- app/src/main/java/com/example/android/itour/ContainerOpenActivity.java
+++ app/src/main/java/com/example/android/itour/ContainerOpenActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,6 +22,8 @@ import butterknife.ButterKnife;
 
 public class ContainerOpenActivity extends AppCompatActivity {
 
+    @BindView(R.id.toolbar_id)
+    Toolbar toolbar;
     @BindView(R.id.place_text)
     TextView place_text;
     @BindView(R.id.top_place_image)
@@ -31,6 +36,10 @@ public class ContainerOpenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container_open);
         ButterKnife.bind(this);
+
+        //Setting Up Toolbar
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
         String Places = intent.getStringExtra("Title");
@@ -56,5 +65,25 @@ public class ContainerOpenActivity extends AppCompatActivity {
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         comment_recyclerview.setLayoutManager(layoutManager);
         comment_recyclerview.setAdapter(commentRecyclerViewAdapter);
+    }
+
+    //Inflating the menu to toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Setting up a Click Listener for the Toolbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/android/itour/TopPlacesOpenActivity.java
+++ app/src/main/java/com/example/android/itour/TopPlacesOpenActivity.java
@@ -3,6 +3,9 @@ package com.example.android.itour;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,6 +14,8 @@ import butterknife.ButterKnife;
 
 public class TopPlacesOpenActivity extends AppCompatActivity {
 
+    @BindView(R.id.toolbar_id)
+    Toolbar toolbar;
     @BindView(R.id.place_text)
     TextView place_text;
     @BindView(R.id.country_text)
@@ -24,6 +29,10 @@ public class TopPlacesOpenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_top_places_open);
         ButterKnife.bind(this);
 
+        //Setting Up Toolbar
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         Intent intent = getIntent();
         String Places = intent.getStringExtra("Places");
         String Country = intent.getStringExtra("Country");
@@ -32,5 +41,25 @@ public class TopPlacesOpenActivity extends AppCompatActivity {
         place_text.setText(Places);
         country_text.setText(Country);
         top_place_image.setImageResource(ImageView);
+    }
+
+    //Inflating the menu to toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Setting up a Click Listener for the Toolbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
